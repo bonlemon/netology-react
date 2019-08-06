ad2a17d1f2edf26319588b959f4efe1c0ec459f0
AVILOV Dmitry <ruaald6@n-msk-macald6.raiffeisen.ru>
2019-08-06 18:03:56 +0300

    events -> layouts

--- events/layouts/src/components/CardsView.js
+++ events/layouts/src/components/CardsView.js
@@ -3,21 +3,13 @@ import React from "react";
 const CardsView = props => {
   const { layout, cards } = props;
   if (layout) {
-    const layoutClasses = Object.keys(layout)
-      .map(key => `col-${key}-${layout[key]}`)
-      .join(" ");
-
     const renderCards = cards => {
       return cards.map((card, i) => {
-        return (
-          <div className={layoutClasses} key={`card-${i}`}>
-            {card}
-          </div>
-        );
+        return <div key={`card-${i}`}>{card}</div>;
       });
     };
     return <div className="row">{renderCards(cards)}</div>;
   }
-  return null
+  return null;
 };
 export default CardsView;
