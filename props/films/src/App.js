717a6f0742d7f39ebf0341dee6f1141dd3762fe9
AVILOV Dmitry <ruaald6@n-msk-macald6.raiffeisen.ru>
2019-08-13 10:36:58 +0300

    add props -> films

---
+++ props/films/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+import Star from "Star";
+
+function App() {
+  return <Stars count={1} />;
+}
+
+export default App;
+``
---
+++ props/films/src/Stars.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./App.css";
+
+function Stars({ count }) {
+  if (count < 0 || count > 5) {
+    return null;
+  }
+  const allStars = [];
+  for (let i = 0; i < count; i++) {
+    allStars.push(
+      <li>
+        <svg
+          fill="#D3BCA2"
+          height="28"
+          viewBox="0 0 18 18"
+          width="28"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z" />
+          <path d="M0 0h18v18H0z" fill="none" />
+        </svg>
+      </li>
+    );
+  }
+  return <ul className="card-body-stars u-clearfix">{allStars}</ul>;
+}
+
+Stars.defaultProps = {
+  count: 0
+};
+
+export default Stars;
