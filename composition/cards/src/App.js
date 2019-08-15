3ceb1a6aa7548c8cc054d81045c5195b3b88e7af
AVILOV Dmitry <ruaald6@n-msk-macald6.raiffeisen.ru>
2019-08-15 12:45:38 +0300

    composition

--- composition/cards/src/App.js
+++ composition/cards/src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Card = ({ srcUrl, title, text, buttonTitle, onClick }) => {
+    return (
+        <div className='card'>
+            {srcUrl && <img alt='chrome' className='card--img' src={srcUrl} />}
+            <div className='card--title'>{title}</div>
+            <div className='card--text'>{text}</div>
+            <button className='card--button' onClick={onClick}>
+                {buttonTitle}
+            </button>
+        </div>
+    );
+};
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className='App'>
+                <Card
+                    title='Cart title 1'
+                    text='Mix and match multiple content types to create the card you need, or throw everything in there. '
+                    buttonTitle='Click'
+                    onClick={() => console.log('1')}
+                    srcUrl='https://jakearchibald.com/static/imgs/browser-icons/chrome.041e39c7c6f7.png'
+                />
+                <Card
+                    title='Cart title 2'
+                    text='Many many words'
+                    buttonTitle='Click'
+                    onClick={() => console.log('1')}
+                />
+            </div>
+        );
+    }
 }
 
-export default App;
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
