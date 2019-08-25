28fca1e172d72618eb5f2f43640a25c45a74f416
AVILOV Dmitry <ruaald6@n-msk-macald6.raiffeisen.ru>
2019-08-25 23:11:20 +0300

    hooks

--- hooks/effect/src/App.js
+++ hooks/effect/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef, Fragment } from 'react';
+import List from './List';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+    const [info, setInfo] = useState({ id: null });
 
-export default App;
+    function handleChoise(id) {
+        setInfo({ id });
+    }
+
+    useEffect(() => {
+        if (info.id) {
+            console.log(info.id);
+
+            console.log(
+                `https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${
+                    info.id.id
+                }.json`
+            );
+            fetch(
+                `https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/${
+                    info.id.id
+                }.json`,
+                { mode: 'cors' }
+            )
+                .then((response) => {
+                    if (response.ok) {
+                        response.json().then((data) => setInfo(data)); //setData
+                    }
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+        }
+    }, [info]);
+
+    return (
+        <div>
+            <List handleChoise={handleChoise} />
+            {info.id ? (
+                <div>
+                    <img src={info.avatar} alt='avatar' />
+                </div>
+            ) : null}
+        </div>
+    );
+}
