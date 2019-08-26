1edd35ede5d3325da68b3738ecf0d381a3d7e2c6
AVILOV Dmitry <ruaald6@n-msk-macald6.raiffeisen.ru>
2019-08-26 23:20:26 +0300

    router

--- router/crud/src/App.js
+++ router/crud/src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import axios from "axios";
+import nanoid from "nanoid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Card from "./components/Card";
+import Create from "./components/Create";
+import CardEdit from "./components/CardEdit";
+
+class App extends React.Component {
+  state = {
+    posts: []
+  };
+  componentDidMount() {
+    this.getPosts();
+  }
+  setData = posts => {
+    this.setState({ posts });
+  };
+  getPosts = () => {
+    axios
+      .get("http://localhost:7777/posts")
+      .then(response => this.setData(response.data));
+  };
+  handleOnRemove = id => e => {
+    axios
+      .delete("http://localhost:7777/posts/" + id)
+      .then(response => this.setData(response.data));
+  };
+  render() {
+    const { posts } = this.state;
+    return (
+      <div className="App">
+        <Router>
+          <Link to="/">
+            <button className="button">Домашняя страница</button>
+          </Link>
+          <Link to="/posts/new">
+            <button className="button">Создать пост</button>
+          </Link>
+          <Switch>
+            <Route
+              path="/posts/new"
+              exact
+              component={() => <Create setData={this.setData} />}
+            />
+            <Route
+              path="/posts/:id"
+              exact
+              component={() => <CardEdit onRemove={this.handleOnRemove} />}
+            />
+            <Route
+              path="/"
+              exact
+              component={() => (
+                <div className="list">
+                  {posts &&
+                    posts.map(post => <Card key={nanoid()} post={post} />)}
+                </div>
+              )}
+            />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
