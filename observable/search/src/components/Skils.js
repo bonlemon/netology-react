fa25bedf0d448b54bc000269d77bc0d15618abdc
AVILOV Dmitry <ruaald6@n-msk-macald6.raiffeisen.ru>
2019-08-29 18:28:59 +0300

    observable -> search

--- observable/search/src/components/Skils.js
+++ observable/search/src/components/Skils.js
@@ -8,23 +8,23 @@ function Skills({ items, loading, error, search, onChange }) {
     onChange(value);
   };
 
-  const hasQuery = search.trim() !== "";
-
   return (
     <Fragment>
       <div>
         <input type="search" value={search} onChange={handleSearch} />
       </div>
-      {!hasQuery && <div>Type something to search</div>}
-      {hasQuery && loading && <div>searching...</div>}
-      {error ? (
-        <div>Error ...</div>
-      ) : (
+      {loading ? (
+        <div>searching...</div>
+      ) : error ? (
+        <div>Error: {error.message}</div>
+      ) : items.length ? (
         <ul>
           {items.map(o => (
             <li key={o.id}>{o.name}</li>
           ))}
         </ul>
+      ) : (
+        <div>Type something to search...</div>
       )}
     </Fragment>
   );
